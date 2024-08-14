4aa07f83aeb9f60746988689efd5db698f19a7c5
alexizxx <sedymov03@bk.ru>
2024-08-14 17:18:06 +0300

    variables and operations

--- hello.pas
+++ hello.pas
@@ -1,6 +1,22 @@
 program hello;
+var
+	x: integer = 500;
+	y: integer;
+	row_x, row_y, row_z : integer;
+	flag: boolean;
 begin
-	writeln('Hello world!')
-	writeln('test')
-end.
+	y := 100;
+	row_x := 10;
+	row_y := 20;
+	row_z := 30;
 
+
+	writeln('Hello, world!');
+	writeln('first');
+	write('second');
+	write('third');
+	writeln('175*113 = ',175*113);
+	writeln('14/7 = ', 14/7:7:2);
+	writeln('27 div 4 = ', 27 div 4, ' and 27 mod 4 = ', 27 mod 4);
+
+end.
